6341147953d72c0bdd4a5be76fb6e0dfe3728b66
ar4sGPT <149362387+ar4sGPT@users.noreply.github.com>
2023-11-05 13:51:23 -0600

    Create kickstart.sh
    Signed-off-by: ar4sGPT <149362387+ar4sGPT@users.noreply.github.com>

---
+++ public/scripts/kickstart.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env/ zsh
+
+## Install w/o Acount
+# curl https://my-netdata.io/kickstart.sh > /tmp/netdata-kickstart.sh && sh /tmp/netdata-kickstart.sh
+
+## Verify Hashes
+# [ "e3281357e8b4a6c2e815ea175c59171e" = "$(curl -Ss https://my-netdata.io/kickstart.sh | md5sum | cut -d ' ' -f 1)" ] && echo "OK, VALID" || echo "FAILED, INVALID"
+
+
